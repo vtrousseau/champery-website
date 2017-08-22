c767186d914eef74085bd4fbcf6e01e9d162ec6c
vtrousseau <vtrousseau@gmail.com>
2017-08-22 23:16:25 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(document).on("scroll", onScroll);
+
   $(window).resize(function() {
     if ($(document).width() > 714)
       $(".responsive-menu").css("height", "0px");
@@ -10,7 +10,7 @@ $(document).ready(function() {
   $(".responsive-menu").append(menu);
   $(".responsive-menu ul").removeClass("main-menu");
   $(".responsive-menu ul").addClass("mobile-menu");
-  $(".menu ul li:first-child").addClass("active");
+  
 
   $(".mobile-menu-icon").click(function() {
 	$(".responsive-menu").toggleClass("menu-border");
@@ -20,33 +20,5 @@ $(document).ready(function() {
       height: height
     }, 100);
   });
-
-  $(".menu-button").click(function(event) {
-        var href = $(this).attr('href');
-	var nav = $('.content-nav');
-if (nav.length) {
-  var contentNav = nav.offset().top;
-    var href = $(this).attr('href');
-    scrollAmount = (href == "#titlepage") ? 0 : $(href).offset().top - 120;
-    $('html, body').animate({
-      scrollTop: scrollAmount
-    }, 1000);
-}
-  });
-
-  function onScroll() {
-    var scrollPos = $(document).scrollTop();
-    $("#test").html(scrollPos);
-    $('.menu li a').each(function() {
-      var currLink = $(this);
-      var refElement = $(currLink).attr("href");
-      if ($(refElement).position().top - 190 <= scrollPos && $(refElement).position().top - 190 + $(refElement).height() >= scrollPos) {
-        currLink.parent().addClass("active");
-
-      } else {
-        currLink.parent().removeClass("active");
-      }
-    });
-  }
 
 });
